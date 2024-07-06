a1c2485f3a9afa1a90c5df181195cd03e016d866
PalomaAndrea <pal.contrerasm@gmail.com>
2024-07-06 02:21:22 -0400

    Se agregan todas las rutas .html en urls.py y se rutean todos los .html en views.py

--- misProyectos/adopta_un_peludo/urls.py
+++ misProyectos/adopta_un_peludo/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
+    path('contacto', views.contacto, name='contacto'),
+    path('donaciones', views.donaciones, name='donaciones'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('tienda', views.tienda, name='tienda'),
 ]
--- misProyectos/adopta_un_peludo/views.py
+++ misProyectos/adopta_un_peludo/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request, 'adopta_un_peludo/index.html', context)
+
+def contacto(request):
+    context={}
+    return render(request, 'adopta_un_peludo/contacto.html', context)
+
+def donaciones(request):
+    context={}
+    return render(request, 'adopta_un_peludo/donaciones.html', context)
+
+def nosotros(request):
+    context={}
+    return render(request, 'adopta_un_peludo/nosotros.html', context)
+
+def tienda(request):
+    context={}
+    return render(request, 'adopta_un_peludo/tienda.html', context)
