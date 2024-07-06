1ff7ef41d6435fb0eb532ef9cc5eff833e9545cf
Paloma Andrea <pal.contreras@duocuc.cl>
2024-07-06 15:27:10 -0400

    Inicio de usuario

--- misProyectos/adopta_un_peludo/admin.py
+++ misProyectos/adopta_un_peludo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Genero, Usuario
+# Register your models here.
+admin.site.register(Genero)
+admin.site.register(Usuario)
--- misProyectos/adopta_un_peludo/models.py
+++ misProyectos/adopta_un_peludo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Genero(models.Model):
+    id_genero = models.AutoField(db_column='idGenero', primary_key=True)
+    genero = models.CharField(max_length=20, blank=False, null=False)
+
+def __str__(self):
+    return str(self.genero)
+
+class Usuario(models.Model):
+    usuario = models.CharField(max_length=20)
+    email = models.EmailField(unique=True, max_length=100, blank=True, null=True)
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    id_genero = models.ForeignKey('Genero',on_delete=models.CASCADE, db_column='idGenero')
+    activo = models.IntegerField()
+
+def __str__(self):
+    return str(self.nombre)+" "+str(self.apellido_paterno)
+
